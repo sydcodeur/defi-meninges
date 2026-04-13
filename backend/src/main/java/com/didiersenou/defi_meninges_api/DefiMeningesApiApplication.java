81b70550fa80da2dc85b314f39913a4b77a27878
Didier Senou <dinhosenou@gmail.com>
2026-04-13 22:25:22 +0200

    chore: initial project setup with monorepo structure

---
+++ backend/src/main/java/com/didiersenou/defi_meninges_api/DefiMeningesApiApplication.java
@@ -0,0 +1,13 @@
+package com.didiersenou.defi_meninges_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefiMeningesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefiMeningesApiApplication.class, args);
+	}
+
+}
